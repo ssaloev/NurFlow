ed496766d2053b58dc1442867f03a2b691888bde
sadi_saloev <sadi_saloev@elon.team>
2025-12-27 20:49:28 +0500

    feat: add pinia

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,9 +2,10 @@ import { App } from "vue";
 import i18n from "@/plugins/i18n";
 import primeVue from "@/plugins/primeVue";
 import router from "@/plugins/router";
+import pinia from "@/plugins/pinia";
 
 export default (app: App) => {
-    const instances = [router, i18n, primeVue];
+    const instances = [pinia, router, i18n, primeVue];
 
     instances.forEach((instance) => {
         instance(app);
