181bb5062130f3c5e38db61810cd9b3a0314795e
sadi_saloev <sadi_saloev@elon.team>
2026-01-04 20:23:25 +0500

    feat: add animation

--- src/pages/difficulties/components/DifficultyCard.vue
+++ src/pages/difficulties/components/DifficultyCard.vue
@@ -12,26 +12,102 @@ const cssClasses = computed(() => {
 </script>
 <template>
     <router-link
-        :to="`/difficulties/${props.id}`"
+        :to="`/difficulties/${props.id}/`"
         :style="`--color: ${props.color}`"
         :class="cssClasses"
     >
         <h3 class="difficulty-card__title">{{ props.name }}</h3>
+        <span
+            class="difficulty-card__border-animation difficulty-card-border-animation difficulty-card-border-animation_top"
+        ></span>
+        <span
+            class="difficulty-card__border-animation difficulty-card-border-animation difficulty-card-border-animation_right"
+        ></span>
+        <span
+            class="difficulty-card__border-animation difficulty-card-border-animation difficulty-card-border-animation_bottom"
+        ></span>
+        <span
+            class="difficulty-card__border-animation difficulty-card-border-animation difficulty-card-border-animation_left"
+        ></span>
     </router-link>
 </template>
 <style lang="scss">
 .difficulty-card {
+    --border-width: 1px;
+    --animation-block-move-to-position: 0px;
+    --animation-block-size: calc(100% + calc(1px + var(--border-width)));
+
+    position: relative;
     width: 100%;
     height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-
-    border: 1px solid var(--color);
+    overflow: hidden;
+    transition: color 0.5s ease-in-out;
 
     &_disabled {
         opacity: 0.6;
         pointer-events: none;
+    }
+
+    &:not(&_disabled):hover,
+    &:not(&_disabled):focus,
+    &:not(&_disabled):active {
+        outline: none;
+        color: var(--color);
+
+        .difficulty-card-border-animation_top {
+            transform: translateX(-100%);
+        }
+
+        .difficulty-card-border-animation_right {
+            transform: translateY(-100%);
+        }
+
+        .difficulty-card-border-animation_bottom {
+            transform: translateX(100%);
+        }
+
+        .difficulty-card-border-animation_left {
+            transform: translateY(100%);
+        }
+    }
+
+    &-border-animation {
+        position: absolute;
+        z-index: 1;
+
+        background-color: var(--color);
+        transition: transform 0.5s ease-in-out;
+    }
+
+    &-border-animation_top {
+        top: var(--animation-block-move-to-position);
+        left: var(--animation-block-move-to-position);
+        height: var(--border-width);
+        width: var(--animation-block-size);
+    }
+
+    &-border-animation_right {
+        right: var(--animation-block-move-to-position);
+        top: var(--animation-block-move-to-position);
+        height: var(--animation-block-size);
+        width: var(--border-width);
+    }
+
+    &-border-animation_bottom {
+        bottom: var(--animation-block-move-to-position);
+        left: var(--animation-block-move-to-position);
+        height: var(--border-width);
+        width: var(--animation-block-size);
+    }
+
+    &-border-animation_left {
+        left: var(--animation-block-move-to-position);
+        top: var(--animation-block-move-to-position);
+        height: var(--animation-block-size);
+        width: var(--border-width);
     }
 }
 </style>
--- src/pages/levels/LevelsPage.vue
+++ src/pages/levels/LevelsPage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div></div>
+    <div>Yo</div>
 </template>
 
 <style scoped lang="scss"></style>
